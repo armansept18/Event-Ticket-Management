ea608a5ca2c4c666019a290d774fdbe929f2652b
arif123 <nashrullah2344@gmail.com>
2023-08-06 17:43:53 +0700

    arif

--- loket/src/pages/login.jsx
+++ loket/src/pages/login.jsx
@@ -53,6 +53,7 @@ export const SimpleCard = ({ users = [] }) => {
       align={"center"}
       justify={"center"}
       bg={useColorModeValue("gray.50", "gray.800")}
+       
     >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
