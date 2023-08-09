3b594c06acf7caaa84d51eaab471c974b5252a89
Arman Septian Pratama <armansept18@gmail.com>
2023-08-09 21:36:50 +0700

    update-navbar

--- loket/src/components/navbar.jsx
+++ loket/src/components/navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar(props) {
       >
         <a
           href="/home"
-        <div
+        
           style={{
             width: "50px",
             height: "50px",
