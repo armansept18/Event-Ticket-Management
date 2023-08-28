d51bfb8a3d3fae71afccf48603f7fb43dc2e0654
arif123 <nashrullah2344@gmail.com>
2023-08-28 09:43:56 +0700

    add top-up and data participants

--- loket/src/redux/reducers/user.js
+++ loket/src/redux/reducers/user.js
@@ -2,11 +2,18 @@ import { types } from "./types";
 
 const init_state = {
   id: 0,
-  name: "",
+  fullname: "",
   email: "",
   password: "",
   referralCode: "",
+<<<<<<< Updated upstream
   credit: "",
+=======
+  referralCodeFromFriend: "",
+  credit: 0,
+  participants: [],
+  isAuthenticated: false,
+>>>>>>> Stashed changes
 };
 
 export const userReducer = (state = init_state, action) => {
@@ -16,8 +23,17 @@ export const userReducer = (state = init_state, action) => {
       id: action.payload.id,
       fullname: action.payload.fullname,
       email: action.payload.email,
+<<<<<<< Updated upstream
       credit: action.payload.credit,
       referralCode: action.payload.referralCode,
+=======
+      password: action.payload.password,
+      referralCode: action.payload.referralCode,
+      referralCodeFromFriend: action.payload.referralCodeFromFriend,
+      credit: action.payload.credit,
+      participants: action.payload.participant,
+      isAuthenticated: action.payload.isAuthenticated,
+>>>>>>> Stashed changes
     };
   } else if (action.type === types.logout) return init_state;
   return state;
