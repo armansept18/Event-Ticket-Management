c468af718f5d06ebb84ac91eeab90c05c9fcb5f2
arif123 <nashrullah2344@gmail.com>
2023-08-28 09:48:03 +0700

    fixing

--- loket/src/components/user-profile.jsx
+++ loket/src/components/user-profile.jsx
@@ -22,21 +22,6 @@ export const UserProfile = () => {
     }
   };
 
-<<<<<<< Updated upstream
-export const UserProfile = ({ user }) => {
-  console.log("user", user);
-  console.log("user.credit", user.credit);
-  return (
-    <Box p={4} borderWidth="1px" borderRadius="md">
-      <Text fontWeight="bold">User Profile</Text>
-      <Text>Name: {user.fullname}</Text>
-      <Text>Email: {user.email}</Text>
-      {user.credit !== undefined && (
-        <Text>Credit: Rp {user.credit.toLocaleString("id-ID")}</Text>
-      )}
-      <Text>Referral Code : {user.referralCode}</Text>
-    </Box>
-=======
   return (
     <>
       <Box p={4} borderWidth="1px" borderRadius="md">
@@ -61,6 +46,5 @@ export const UserProfile = ({ user }) => {
         </Button>
       </Box>
     </>
->>>>>>> Stashed changes
   );
 };
--- loket/src/redux/reducers/user.js
+++ loket/src/redux/reducers/user.js
@@ -6,14 +6,11 @@ const init_state = {
   email: "",
   password: "",
   referralCode: "",
-<<<<<<< Updated upstream
   credit: "",
-=======
   referralCodeFromFriend: "",
   credit: 0,
   participants: [],
   isAuthenticated: false,
->>>>>>> Stashed changes
 };
 
 export const userReducer = (state = init_state, action) => {
@@ -23,17 +20,14 @@ export const userReducer = (state = init_state, action) => {
       id: action.payload.id,
       fullname: action.payload.fullname,
       email: action.payload.email,
-<<<<<<< Updated upstream
       credit: action.payload.credit,
       referralCode: action.payload.referralCode,
-=======
       password: action.payload.password,
       referralCode: action.payload.referralCode,
       referralCodeFromFriend: action.payload.referralCodeFromFriend,
       credit: action.payload.credit,
       participants: action.payload.participant,
       isAuthenticated: action.payload.isAuthenticated,
->>>>>>> Stashed changes
     };
   } else if (action.type === types.logout) return init_state;
   return state;
